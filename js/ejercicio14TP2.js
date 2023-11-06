6f5c30547f016880dd83566f2659f63a7053c27f
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-11-06 02:02:10 -0300

    se realizó un programa donde se pide una cadena de texto y se separa cada carácter con un guión

---
+++ js/ejercicio14TP2.js
@@ -0,0 +1,9 @@
+/*
+Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+*/
+
+let letras = prompt("Ingrese un texto:");
+let cadena = letras.split("");
+let resultado = cadena.join("-");
+
+alert(resultado);
